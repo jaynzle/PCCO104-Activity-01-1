5bda4696e42c8c16c994a7e04710b0974d80928c
jaynzle <158656853+jaynzle@users.noreply.github.com>
2024-02-03 03:11:32 +0000

    age

--- main.cs
+++ main.cs
@@ -4,7 +4,8 @@ class Program {
   public static void Main (string[] args) {
     string firstName = "Nathan";
     string lastName = "Esmaya";
-    Console.WriteLine ($"Hello World {firstName} {lastName}");
-    Console.WriteLine ("Hello World" + " " + firstName + " " + lastName);
+    int age = 19;
+    Console.WriteLine ($"Hello World {firstName} {lastName} {age}");
+    Console.WriteLine ("Hello World" + " " + firstName + " " + lastName + " " + age);
   }
 }
